0e3096abf3248455d6d625380af3db9564bb62e4
Ouhfi <ouhfid4@gmail.com>
2026-06-05 16:26:15 +0100

    page home dnot style

---
+++ Data/data.js
@@ -0,0 +1,38 @@
+export const sportsData = [
+  {
+    id: "1",
+    title: "Football",
+    description: "The most popular team sport in the world",
+    image: require("../assets/images/football.jpg"),
+  },
+  {
+    id: "2",
+    title: "Basketball",
+    description: "Fast-paced and spectacular team sport",
+    image: require("../assets/images/basketball.jpg"),
+  },
+  {
+    id: "3",
+    title: "Tennis",
+    description: "Individual or doubles racket sport",
+    image: require("../assets/images/tennis.jpg"),
+  },
+  {
+    id: "4",
+    title: "Swimming",
+    description: "Complete aquatic sport",
+    image: require("../assets/images/swimming.jpg"),
+  },
+  {
+    id: "5",
+    title: "Athletics",
+    description: "Running, jumping, and throwing events",
+    image: require("../assets/images/athletics.jpg"),
+  },
+  {
+    id: "6",
+    title: "Cycling",
+    description: "Endurance sport on road or track",
+    image: require("../assets/images/cycling.jpg"),
+  },
+];
